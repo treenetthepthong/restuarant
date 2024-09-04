05f6bef81340146d2e9ed37e2297e3f4079fb830
jitnupong-b <jitnupong.b@ku.th>
2024-09-04 17:29:50 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { LoginComponent } from './admin/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
@@ -12,7 +13,9 @@ const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'about-us', component: AboutUsComponent }, 
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'reservation', component: ReservationComponent }
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'admin/admin-login', component: LoginComponent },
+  { path: 'admin', redirectTo: 'admin/admin-login', pathMatch: 'full' }
 ];
 
 @NgModule({
