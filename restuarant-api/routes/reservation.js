c133f7c6eeee3a2f266b2c13d3c3dced6af4dcdb
boonchoojit <jitnupong.b@ku.th>
2024-09-23 10:57:42 +0700

    edit reservation router

--- restuarant-api/routes/reservation.js
+++ restuarant-api/routes/reservation.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var config = require("../config/dbconfig");
 const sql = require("mssql");
 
 /* GET reservation listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 router.post("/addReservation", async function (req, res, next) {
@@ -33,8 +33,47 @@ router.post("/addReservation", async function (req, res, next) {
 router.get("/getReservation", async function (req, res, next) {
   try {
     let pool = await sql.connect(config);
-    let result = await pool.request().query("SELECT * FROM tbl_reservation_table");
+    let result = await pool
+      .request()
+      .query("SELECT * FROM tbl_reservation_table");
     return res.status(200).json({ data: result.recordset });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ data: err });
+  }
+});
+
+router.get("/getReservation/:id", async function (req, res, next) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("id", sql.Int, req.params.id)
+      .query("SELECT * FROM tbl_reservation_table WHERE id = @id");
+    return res.status(200).json({ data: result.recordset });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ data: err });
+  }
+});
+
+router.put("/updateReservation/:id", async function (req, res, next) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("id", sql.Int, req.params.id)
+      .input("guestName", sql.VarChar, req.body.name)
+      .input("guestEmail", sql.VarChar, req.body.email)
+      .input("guestTel", sql.VarChar, req.body.phone)
+      .input("guestPax", sql.Int, req.body.guests)
+      .input("guestRemarks", sql.VarChar, req.body.specialRequests)
+      .input("guestDate", sql.Date, req.body.reserveDate)
+      .input("guestTime", sql.VarChar, req.body.reserveTime)
+      .query(
+        "UPDATE tbl_reservation_table SET name = @guestName, email = @guestEmail, tel = @guestTel, pax = @guestPax, remarks = @guestRemarks, reserve_date = @guestDate, reserve_time = CAST(@guestTime AS TIME) WHERE id = @id"
+      );
+    return res.status(200).json({ data: result });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ data: err });
